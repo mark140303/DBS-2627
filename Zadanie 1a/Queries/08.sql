8001c3584d1cd8778b58645849970ee089fd2c7b
mark140303 <ipolyimarkk@gmail.com>
2026-09-24 00:04:32 +0200

    08

---
+++ Zadanie 1a/Queries/08.sql
@@ -0,0 +1,6 @@
+SELECT c.customer_name,
+       COUNT(o.order_id) AS order_count
+FROM customers c
+LEFT JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.customer_name
+ORDER BY order_count DESC;
