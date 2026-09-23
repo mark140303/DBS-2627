3058a3f8fdf39c0956849c2b1fd6827e249e7835
mark140303 <ipolyimarkk@gmail.com>
2026-09-23 23:57:06 +0200

    03

---
+++ Zadanie 1a/Queries/03.sql
@@ -0,0 +1,7 @@
+SELECT o.order_id,
+       c.customer_name,
+       p.category,
+       o.sales
+FROM orders o
+INNER JOIN customers c ON o.customer_id = c.customer_id
+INNER JOIN products  p ON o.product_id  = p.product_id;
